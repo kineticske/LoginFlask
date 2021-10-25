60eeed4f91a7f9f2301f0ae01741786e1b2250d9
Kineticske <developverphy@gmail.com>
2021-10-25 15:57:03 -0500

    inicio sencilo y logout sin validacion

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 
-from flask import Flask, render_template, sessions, url_for, redirect, jsonify, session
+from flask import Flask, render_template, sessions, url_for, redirect, jsonify, session, request, redirect
 from werkzeug.exceptions import  abort 
 from werkzeug.utils import  redirect
 
@@ -10,9 +10,19 @@ app.secret_key='secret_key_bp'
 @app.route('/')
 def Inicio():
     if 'username' in session:
-        return 'el usuario ha hecho login'
+        return f'el usuario  {session["username"] } ha hecho login'
     return 'No se ha logrado el login'
 
 @app.route('/login', methods=['GET', 'POST'])
 def Login():
+    if request.method=='POST':
+        usuario=request.form['username']
+        session['username']=usuario
+        return redirect(url_for('Inicio'))
+    
     return render_template('login.html')
+
+@app.route('/logout/')
+def logout():
+    session.pop('username')
+    return redirect(url_for('Inicio'))
